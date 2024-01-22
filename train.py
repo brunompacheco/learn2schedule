a2aae045e9a7095c9bff6a225207197a7ad06f3f
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2024-01-22 14:55:25 -0300

    Initial commit

---
+++ train.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+from src.dataset import MyDataset
+
+from src.net import Network
+from src.trainer import Trainer
+from src.utils import debugger_is_active
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    if debugger_is_active():
+        import random
+        seed = 33
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        # torch.use_deterministic_algorithms(True)
+
+        wandb_project = None  # avoid logging run
+
+        torch.autograd.set_detect_anomaly(True)
+    else:
+        seed = None
+        # wandb_project = 'PROJECT-NAME'
+
+    Trainer(
+        Network(),
+        MyDataset(),
+        wandb_project=wandb_project,
+        random_seed=seed,
+        device=device,
+    ).run()
