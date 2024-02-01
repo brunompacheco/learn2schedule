88bad70160e1a3f7b86ef3ed428598594f9e93a9
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2024-02-01 12:03:23 -0300

    test training MLP for ONTS

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 import numpy as np
 import torch
-from src.dataset import MyDataset
 
-from src.net import Network
+from src.dataset import ONTSFeatures
+from src.net import ONTSMLP
 from src.trainer import Trainer
 from src.utils import debugger_is_active
 
@@ -24,11 +26,27 @@ if __name__ == '__main__':
     else:
         seed = None
         # wandb_project = 'PROJECT-NAME'
+        wandb_project = "milp-solution-prediction"
+
+    J = 9
+    opt_dir = Path('data/interim/ONTS')
+    instances_dir = Path('data/raw/ONTS')
+    train_instances = [fp for fp in instances_dir.glob('*.json')
+                       if (int(fp.stem.split('_')[1]) == J)
+                       and (int(fp.stem.split('_')[2]) < 160)]
+    val_instances = [fp for fp in instances_dir.glob('*.json')
+                     if (int(fp.stem.split('_')[1]) == J)
+                     and (int(fp.stem.split('_')[2]) >= 160)]
+    train_dataset = ONTSFeatures(train_instances, opt_dir)
+    val_dataset = ONTSFeatures(val_instances, opt_dir)
 
     Trainer(
-        Network(),
-        MyDataset(),
+        ONTSMLP(J, 125, 11, 3, 50).double(),
+        train_dataset=train_dataset,
+        val_dataset=val_dataset,
         wandb_project=wandb_project,
+        wandb_group="TEST",
+        epochs=20,
         random_seed=seed,
         device=device,
     ).run()
